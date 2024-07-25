94a8fbbbdbcd6b44492cef4016e06a52c4d9fabd
Biplov32 <biplovbelbase30@gmail.com>
2024-07-26 00:38:51 +0545

    Third day

---
+++ QR_Codegeneration.py
@@ -0,0 +1,11 @@
+import qrcode as qr
+from PIL import Image
+# img = qr.make('https://www.linkedin.com/in/biplov-belbase/')
+# img.save('linkdenqr.jpeg')
+qrc = qr.QRCode(version=1,error_correction=qr.constants.ERROR_CORRECT_H,box_size=10,border=4)
+
+qrc.add_data("https://www.linkedin.com/in/biplov-belbase/")
+qrc.make(fit=True)
+
+img=qrc.make_image(fill_color="blue",back_color="white")
+img.save('linkden2.jpeg')
